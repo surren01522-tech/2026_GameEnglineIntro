f5e7876fb9b29d76d653cb833b93ef169ead8c71
moonjiyu <surren01522@gmail.com>
2026-03-18 16:07:58 +0900

    Add:플레이어 기본 움직임

--- My project (4)/Assets/playercontrolier.cs
+++ My project (4)/Assets/playercontrolier.cs
@@ -6,11 +6,32 @@ public class playercontrolier : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
     {
-        moveInput=value.Get<Vector2>();
+        moveInput = value.Get<Vector2>();
     }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed) // 점프 버튼을 누르면
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -21,6 +42,13 @@ public class playercontrolier : MonoBehaviour
         else if(moveInput.x<0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(moveInput.magnitude > 0) {
+            myAnimator.SetBool("move", true);
+        }
+        else {
+            myAnimator.SetBool("move", false);
         }
         transform.Translate(Vector3.right  * moveSpeed * moveInput.x * Time.deltaTime);
     }
